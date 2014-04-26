a9a2bbd88fc2bf36cedbcfc1ef407dadee9d4f4f
Jonathan Neal <jonathantneal@hotmail.com>
2014-04-26 14:10:53 -0700

    Update Chrome extension
    Add “Create fragmention” context menu item on text selection
    Bump Chrome version

---
+++ chrome-extension/background.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/* global chrome */
+
+chrome.contextMenus.create({
+	title: 'Create fragmention',
+	contexts: ['selection'],
+	onclick: function (event) {
+		if (chrome.tabs) {
+			chrome.tabs.query({
+				active: true,
+				currentWindow: true
+			}, function (tabs) {
+				var
+				tab = tabs[0],
+				url = tab.url.replace(/#.*$/, ''),
+				text = event.selectionText,
+				hash = '##' + encodeURI(text.trim().replace(/\s+/g, '+'));
+
+				chrome.tabs.update(tab.id, {
+					url: url + hash
+				});
+			});
+		}
+	}
+});
